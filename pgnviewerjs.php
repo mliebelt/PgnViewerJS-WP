f239c5940450814c5296ad132ec72e72c2886e02
Markus Liebelt <mliebelt@hey.com>
2025-01-03 15:59:44 +0100

    #139 Allowed all 4 shortcodes again, added some more attributes to the form

--- pgnviewerjs.php
+++ pgnviewerjs.php
@@ -23,13 +23,18 @@ add_action('plugins_loaded', 'pgn_viewer_load_textdomain');
 function pgn_viewer_enqueue_assets(): void {
 
     // Enqueue scripts and styles conditionally
-//    if (has_shortcode(get_post()->post_content ?? '', 'pgnv') ||
-//        has_shortcode(get_post()->post_content ?? '', 'pgne') ||
-//        has_shortcode(get_post()->post_content ?? '', 'pgnb') ||
-//        has_shortcode(get_post()->post_content ?? '', 'pgnp')) {
+    global $post;
+    if ($post && (
+        has_shortcode($post->post_content, 'pgnv') ||
+        has_shortcode($post->post_content, 'pgne') ||
+        has_shortcode($post->post_content, 'pgnb') ||
+        has_shortcode($post->post_content, 'pgnp') ||
+        has_block('pgn-viewer/block-editor', $post->post_content)
+    )) {
         wp_enqueue_script('pgnviewerjs');
         wp_enqueue_style('pgnviewerjs-styles');
-//    }
+    }
+
     wp_register_script(
         'pgnviewerjs', plugins_url('js/dist.js', __FILE__), [], '1.6.10', true);
     wp_register_style(
@@ -38,11 +43,28 @@ function pgn_viewer_enqueue_assets(): void {
 add_action('wp_enqueue_scripts', 'pgn_viewer_enqueue_assets');
 add_action('enqueue_block_editor_assets', 'pgn_viewer_enqueue_scripts');
 
+// helper for formatting the  attributes in JSON
+function format_attribute_value($key, $value) {
+    $boolean_attributes = ['headers', 'showCoords', 'coordsInner', 'resizable'];
+    $integer_attributes = ['timertime', 'coordsFontSize'];
+    $float_attributes = ['coordsFactor'];
+
+    if (in_array($key, $boolean_attributes)) {
+            return $value ? 'true' : 'false';
+        } elseif (in_array($key, $integer_attributes)) {
+            return is_numeric($value) ? (int)$value : 'null';
+        } elseif (in_array($key, $float_attributes)) {
+            return is_numeric($value) ? (float)$value : 'null';
+        } else {
+            return json_encode($value);
+        }
+}
+
 function pgn_viewer_render(array $attributes, string $content = '', string $mode = 'pgnView'): string {
     ob_start();
     // Extract block attributes
     $position = $attributes['position'] ?? '';
-    $pgn = $content;
+    $pgn = $content ?: ''; // Use empty string if content is null or empty
     $layout = $attributes['layout'] ?? 'left';
     $orientation = $attributes['orientation'] ?? 'white';
     $piecestyle = $attributes['piecestyle'] ?? 'merida';
@@ -52,73 +74,103 @@ function pgn_viewer_render(array $attributes, string $content = '', string $mode
     $movesheight = $attributes['movesheight'] ?? null;
     $moveswidth = $attributes['moveswidth'] ?? null;
     $timertime = $attributes['timertime'] ?? null;
-    $notationlayout = $attributes['notationlayout'] ?? 'inline';
+    $notationLayout = $attributes['notationLayout'] ?? 'inline';
     $notation = $attributes['notation'] ?? 'short';
     $headers = $attributes['headers'] ?? true;
-    $showcoords = $attributes['showcoords'] ?? true;
-    $coordsinner = $attributes['coordsinner'] ?? true;
+    $showCoords = isset($attributes['showCoords']) ? filter_var($attributes['showCoords'], FILTER_VALIDATE_BOOLEAN) : true;
+    $coordsInner = isset($attributes['coordsInner']) ? filter_var($attributes['coordsInner'], FILTER_VALIDATE_BOOLEAN) : true;
+    $coordsFactor = $attributes['coordsFactor'] ?? 1;
+    $coordsFontSize = $attributes['coordsFontSize'] ?? null;
     $figurine = $attributes['figurine'] ?? null;
     $locale = $attributes['locale'] ?? 'en';
+    $resizable = $attributes['resizable'] ?? true;
+    $colorMarker = $attributes['colorMarker'] ?? null;
+    $figurine = $attributes['figurine'] ?? '';
+
 
     // Generate unique ID for the block
     $id = $attributes['id'] ?? generate_random_string();
 
     // Build config options passed to the viewer
-    $config = array_filter([
-        'position' => $position,
-        'pgn' => $pgn,
-        'orientation' => $orientation,
-        'pieceStyle' => $piecestyle,
-        'theme' => $theme,
-        'boardSize' => $boardsize,
-        'width' => $width,
-        'movesHeight' => $movesheight,
-        'movesWidth' => $moveswidth,
-        'notationLayout' => $notationlayout, // inline / list
-        'timerTime' => $timertime, // in millis
-        'notation' => $notation, // short / long
-        'headers' => $headers,
-        'showCoords' => $showcoords,
-        'coordsInner' => $coordsinner,
-        'layout' => $layout, // left / right / top / bottom
-        'figurine' => $figurine, // alpha / merida / berlin / noto / cachess
-        'locale' => $locale,
-    ]);
+        $config = array_filter([
+            'position' => $position,
+            'pgn' => $pgn,
+            'orientation' => $orientation,
+            'pieceStyle' => $piecestyle,
+            'theme' => $theme,
+            'boardSize' => $boardsize,
+            'width' => $width,
+            'movesHeight' => $movesheight,
+            'movesWidth' => $moveswidth,
+            'notationLayout' => $notationLayout,
+            'timerTime' => $timertime,
+            'notation' => $notation,
+            'headers' => $headers,
+            'showCoords' => $showCoords,
+            'coordsInner' => $coordsInner,
+            'coordsFactor' => $coordsFactor,
+            'coordsFontSize' => $coordsFontSize,
+            'layout' => $layout,
+            'figurine' => $figurine,
+            'locale' => $locale,
+            'resizable' => $resizable,
+            'colorMarker' => $colorMarker,
+            'figurine' => $figurine,
+        ], function($value) { return $value !== null && $value !== ''; });
 
-    // Generate the layout structure with attributes
-    $layout = '';
-    $configString = '';
-    foreach ($config as $key => $value) {
-        $configString .= sprintf("\"%s\": \"%s\" ,", esc_attr($key), esc_attr($value));
-    }
-    // error_log('Config string is: ' . $configString);
+    // Modify the JavaScript initialization based on the mode
+        $jsFunction = $mode;
+
+        // Generate the config string with proper type handling
+        $configString = '';
+        foreach ($config as $key => $value) {
+            $formattedValue = format_attribute_value($key, $value);
+            $configString .= sprintf('"%s": %s,', esc_attr($key), $formattedValue);
+        }
+        $configString = rtrim($configString, ','); // Remove trailing comma
 
 
-    // Render the PGN Viewer block
-    $output = sprintf(
-        '<div id="%s" class="pgn-viewer-block-wrapper"></div>
-        <script type="application/javascript">
-            setTimeout(function () {
-                if (typeof PGNV !== "undefined") {
-                    console.log("Initializing PGNV");
-                    PGNV.pgnView("%s", { %s });
-                } else {
-                    console.error("PGNV is not loaded properly!");
-                }
-            }, 500);
-        </script>',
-        esc_attr($id), // ID
-        esc_attr($id), // Viewer JS initialization
-        $configString
-    );
-    return $output;
+        // Render the PGN Viewer block
+        $output = sprintf(
+            '<div id="%s" class="pgn-viewer-block-wrapper"></div>
+            <script type="application/javascript">
+                setTimeout(function () {
+                    if (typeof PGNV !== "undefined") {
+                        console.log("Initializing PGNV");
+                        PGNV.%s("%s", { %s });
+                    } else {
+                        console.error("PGNV is not loaded properly!");
+                    }
+                }, 100);
+            </script>',
+            esc_attr($id),
+            $mode,
+            esc_attr($id),
+            $configString
+        );
+        return $output;
 }
 
 // Shortcode callbacks
 function pgn_viewer_shortcode(array $attributes, string $content = ''): string {
     return pgn_viewer_render($attributes, $content, 'pgnView');
 }
+
+function pgn_board_shortcode(array $attributes, string $content = ''): string {
+    return pgn_viewer_render($attributes, $content, 'pgnBoard');
+}
+
+function pgn_edit_shortcode(array $attributes, string $content = ''): string {
+    return pgn_viewer_render($attributes, $content, 'pgnEdit');
+}
+
+function pgn_print_shortcode(array $attributes, string $content = ''): string {
+    return pgn_viewer_render($attributes, $content, 'pgnPrint');
+}
 add_shortcode('pgnv', 'pgn_viewer_shortcode');
+add_shortcode('pgnb', 'pgn_board_shortcode');
+add_shortcode('pgne', 'pgn_edit_shortcode');
+add_shortcode('pgnp', 'pgn_print_shortcode');
 
 // Utility: PGN Cleaning
 function cleanup_pgnv(string $string): string {
@@ -173,5 +225,11 @@ function pgn_viewer_register_block() {
 add_action('init', 'pgn_viewer_register_block');
 
 function pgn_viewer_render_block($attributes, $content) {
-    return pgn_viewer_render($attributes, $attributes['pgn'], 'pgnView');
+    $mode = $attributes['mode'] ?? 'view';
+    $jsFunction = 'pgn' . ucfirst($mode); // This will create pgnView, pgnEdit, pgnPrint, or pgnBoard
+
+    // Use content if pgn is not set in attributes
+    $pgn = $attributes['pgn'] ?? $content;
+
+    return pgn_viewer_render($attributes, $pgn, $jsFunction);
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
-const { SelectControl, TextControl, TextareaControl } = wp.components;
+const {
+  SelectControl,
+  TextControl,
+  TextareaControl,
+  Panel,
+  PanelBody,
+  CheckboxControl,
+} = wp.components;
 const { useBlockProps } = wp.blockEditor;
 
 // Register the block
@@ -9,29 +16,59 @@ registerBlockType("pgn-viewer/block-editor", {
   icon: "chess",
   category: "widgets",
   attributes: {
+    mode: { type: "string", default: "view" },
+    layout: { type: "string", default: "top" },
     position: { type: "string", default: "" },
     pgn: { type: "string", default: "" },
     orientation: { type: "string", default: "white" },
-    piecestyle: { type: "string", default: "merida-gradient" },
+    piecestyle: { type: "string", default: "merida" },
     theme: { type: "string", default: "zeit" },
     boardsize: { type: "string", default: "400px" },
     width: { type: "string", default: "500px" },
     movesHeight: { type: "string", default: "" },
+    showCoords: { type: "boolean", default: true },
+    coordsInner: { type: "boolean", default: true },
+    coordsFactor: { type: "number", default: 1 },
+    coordsFontSize: { type: "number", default: 14 },
+    colorMarker: { type: "string", default: "" },
+    locale: { type: "string", default: "en" },
+    figurine: { type: "string", default: "" },
+    notationLayout: { type: "string", default: "inline" },
   },
   edit: ({ attributes, setAttributes }) => {
+    // console.log("Current attributes:", attributes);
     const blockProps = useBlockProps({
       className: "pgnv-wrapper",
     });
 
+    const onChangeShowCoords = (value) => {
+      console.log("Changing showCoords to:", value);
+      setAttributes({ showCoords: value });
+    };
+
     return (
       <div {...blockProps}>
-        {/* Group 1: Full-width Rows */}
-        <div className="row group-1">
+        {/* Mode Selector */}
+        <div className="row group-12">
+          <SelectControl
+            label={__("Mode", "pgn-viewer")}
+            value={attributes.mode}
+            options={[
+              { label: __("View", "pgn-viewer"), value: "view" },
+              { label: __("Edit", "pgn-viewer"), value: "edit" },
+              { label: __("Print", "pgn-viewer"), value: "print" },
+              { label: __("Board", "pgn-viewer"), value: "board" },
+            ]}
+            onChange={(mode) => setAttributes({ mode })}
+          />
           <TextControl
             label={__("FEN", "pgn-viewer")}
             value={attributes.position}
             onChange={(position) => setAttributes({ position })}
           />
+        </div>
+        {/* Group 1: Full-width Rows */}
+        <div className="row group-1">
           <TextareaControl
             label={__("PGN", "pgn-viewer")}
             value={attributes.pgn}
@@ -39,28 +76,30 @@ registerBlockType("pgn-viewer/block-editor", {
           />
         </div>
 
-        {/* Group 3: Three equally spaced controls */}
+        {/* Group 3: Three equally spaced controls.
+         resizable, colorMarker */}
         <div className="row group-3">
-          <SelectControl
+          <CheckboxControl
             label={__("Orientation", "pgn-viewer")}
-            value={attributes.orientation}
-            options={[
-              { label: __("White", "pgn-viewer"), value: "white" },
-              { label: __("Black", "pgn-viewer"), value: "black" },
-            ]}
-            onChange={(orientation) => setAttributes({ orientation })}
+            help={__("Check for white, uncheck for black", "pgn-viewer")}
+            checked={attributes.orientation === "white"}
+            onChange={(isChecked) =>
+              setAttributes({ orientation: isChecked ? "white" : "black" })
+            }
           />
           <SelectControl
             label={__("Piece Style", "pgn-viewer")}
             value={attributes.piecestyle}
             options={[
-              { label: __("Merida", "pgn-viewer"), value: "merida-gradient" },
-              {
-                label: __("Goodcomp", "pgn-viewer"),
-                value: "goodcomp-gradient",
-              },
+              { label: __("Merida", "pgn-viewer"), value: "merida" },
+              { label: __("Wikipedia", "pgn-viewer"), value: "wikipedia" },
               { label: __("Alpha", "pgn-viewer"), value: "alpha" },
               { label: __("Leipzig", "pgn-viewer"), value: "leipzig" },
+              { label: __("USCF", "pgn-viewer"), value: "uscf" },
+              { label: __("Case", "pgn-viewer"), value: "case" },
+              { label: __("Condal", "pgn-viewer"), value: "condal" },
+              { label: __("Maya", "pgn-viewer"), value: "maya" },
+              { label: __("Beyer", "pgn-viewer"), value: "beyer" },
             ]}
             onChange={(piecestyle) => setAttributes({ piecestyle })}
           />
@@ -69,29 +108,165 @@ registerBlockType("pgn-viewer/block-editor", {
             value={attributes.theme}
             options={[
               { label: __("Zeit", "pgn-viewer"), value: "zeit" },
-              { label: __("Dark", "pgn-viewer"), value: "dark" },
-              { label: __("Light", "pgn-viewer"), value: "light" },
+              { label: __("Green", "pgn-viewer"), value: "green" },
+              { label: __("Blue", "pgn-viewer"), value: "blue" },
+              { label: __("Falken", "pgn-viewer"), value: "falken" },
+              { label: __("Beyer", "pgn-viewer"), value: "beyer" },
+              { label: __("Sportverlag", "pgn-viewer"), value: "sportverlag" },
+              { label: __("Informator", "pgn-viewer"), value: "informator" },
+              { label: __("Brown", "pgn-viewer"), value: "brown" },
             ]}
             onChange={(theme) => setAttributes({ theme })}
           />
         </div>
 
-        {/* Group 3: Board size, width, and moves height */}
-        <div className="row group-3">
-          <TextControl
-            label={__("Board Size", "pgn-viewer")}
-            value={attributes.boardsize}
-            onChange={(boardsize) => setAttributes({ boardsize })}
+        {/* Group 12: Layout and Width */}
+        <div className="row group-12">
+          <SelectControl
+            label={__("Layout", "pgn-viewer")}
+            value={attributes.layout}
+            options={[
+              { label: __("Top", "pgn-viewer"), value: "top" },
+              { label: __("Bottom", "pgn-viewer"), value: "bottom" },
+              { label: __("Left", "pgn-viewer"), value: "left" },
+              { label: __("Right", "pgn-viewer"), value: "right" },
+            ]}
+            onChange={(layout) => setAttributes({ layout })}
           />
           <TextControl
             label={__("Width", "pgn-viewer")}
             value={attributes.width}
             onChange={(width) => setAttributes({ width })}
           />
-          <TextControl
-            label={__("Moves Height", "pgn-viewer")}
-            value={attributes.movesHeight}
-            onChange={(movesHeight) => setAttributes({ movesHeight })}
+        </div>
+
+        {/* Expandable Advanced Settings */}
+        <Panel>
+          <PanelBody
+            title={__("Board Settings", "pgn-viewer")}
+            initialOpen={false}
+          >
+            <div className="row group-2">
+              <TextControl
+                label={__("Board Size", "pgn-viewer")}
+                value={attributes.boardsize}
+                onChange={(boardsize) => setAttributes({ boardsize })}
+              />
+              <TextControl
+                label={__("Moves Height", "pgn-viewer")}
+                value={attributes.movesHeight}
+                onChange={(movesHeight) => setAttributes({ movesHeight })}
+              />
+            </div>
+            <div className="row group-2">
+              <CheckboxControl
+                label={__("Resizable", "pgn-viewer")}
+                help={__("Allow the reader to resize the board", "pgn-viewer")}
+                checked={attributes.resizable == true}
+                onChange={(resizable) => setAttributes({ resizable })}
+              />
+              <SelectControl
+                label={__("Color Marker", "pgn-viewer")}
+                value={attributes.colorMarker}
+                options={[
+                  { label: __("None", "pgn-viewer"), value: "" },
+                  { label: __("Any", "pgn-viewer"), value: "any" },
+                  { label: __("Circle", "pgn-viewer"), value: "circle" },
+                  {
+                    label: __("Circle Small", "pgn-viewer"),
+                    value: "circle-small",
+                  },
+                  {
+                    label: __("Circle Big", "pgn-viewer"),
+                    value: "circle-big",
+                  },
+                  {
+                    label: __("Square Small", "pgn-viewer"),
+                    value: "cm-small",
+                  },
+                  { label: __("Circle Big", "pgn-viewer"), value: "cm-big" },
+                ]}
+                onChange={(colorMarker) => setAttributes({ colorMarker })}
+              />
+            </div>
+          </PanelBody>
+        </Panel>
+
+        {/* Expandable Advanced Settings */}
+        <Panel>
+          <PanelBody
+            title={__("Coordinate Settings", "pgn-viewer")}
+            initialOpen={false}
+          >
+            <div className="row group-2">
+              <CheckboxControl
+                label={__("Show Coordinates", "pgn-viewer")}
+                checked={attributes.showCoords}
+                onChange={onChangeShowCoords}
+              />
+              <CheckboxControl
+                label={__("Coordinates Inner", "pgn-viewer")}
+                checked={attributes.coordsInner}
+                onChange={(isChecked) =>
+                  setAttributes({ coordsInner: isChecked })
+                }
+              />
+              <TextControl
+                label={__("Coords Factor", "pgn-viewer")}
+                type="number"
+                value={attributes.coordsFactor}
+                onChange={(value) =>
+                  setAttributes({ coordsFactor: parseFloat(value) })
+                }
+                step="0.1" // This allows decimal inputs
+              />
+              <TextControl
+                label={__("Coords Font Size", "pgn-viewer")}
+                type="number"
+                value={attributes.coordsFontSize}
+                onChange={(value) =>
+                  setAttributes({ coordsFontSize: parseInt(value) })
+                }
+              />
+            </div>
+          </PanelBody>
+        </Panel>
+
+        {/* moves width, moves height, header?, notation, notationLayout, timerTime, showResult, timeAnnotation */}
+        <div className="row group-3">
+          <SelectControl
+            label={__("Locale", "pgn-viewer")}
+            value={attributes.locale}
+            options={[
+              { label: __("EN", "pgn-viewer"), value: "en" },
+              { label: __("DE", "pgn-viewer"), value: "de" },
+              { label: __("FR", "pgn-viewer"), value: "fr" },
+              { label: __("ES", "pgn-viewer"), value: "es" },
+              { label: __("IT", "pgn-viewer"), value: "it" },
+            ]}
+            onChange={(locale) => setAttributes({ locale })}
+          />
+          <SelectControl
+            label={__("Figurine", "pgn-viewer")}
+            value={attributes.figurine}
+            options={[
+              { label: __("None", "pgn-viewer"), value: "" },
+              { label: __("Alpha", "pgn-viewer"), value: "alpha" },
+              { label: __("Merida", "pgn-viewer"), value: "merida" },
+              { label: __("Berlin", "pgn-viewer"), value: "berlin" },
+              { label: __("Noto", "pgn-viewer"), value: "noto" },
+              { label: __("Cachess", "pgn-viewer"), value: "cachess" },
+            ]}
+            onChange={(figurine) => setAttributes({ figurine })}
+          />
+          <SelectControl
+            label={__("Notation Layout", "pgn-viewer")}
+            value={attributes.notationLayout}
+            options={[
+              { label: __("Inline", "pgn-viewer"), value: "inline" },
+              { label: __("List", "pgn-viewer"), value: "list" },
+            ]}
+            onChange={(notationLayout) => setAttributes({ notationLayout })}
           />
         </div>
       </div>
